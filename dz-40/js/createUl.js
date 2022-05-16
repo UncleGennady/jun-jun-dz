15571580d986c39901ecc68cbd26900a50a1b830
Aleksandr Protasenko <96978077+UncleGennady@users.noreply.github.com>
2022-05-16 20:37:49 +0300

    update

--- dz-40/js/createUl.js
+++ dz-40/js/createUl.js
@@ -1,40 +1,19 @@
 'use strict';
-void function () {
-    const dataKey = 'formData';
-    let data = localStorage.getItem(dataKey);
- 
+void function (){
+    let data = localStorage.getItem('formData');
     data = JSON.parse(data);
 
-    if (data === null){
-       return alert ('нет данных в localStorage')
+    if(data === null) return alert('нет данных в localStorage');
+
+    const ul = document.createElement('ul');
+    document.body.prepend(ul);
+    
+    for (const key in data) {
+
+        const li = document.createElement('li');
+        ul.append(li);
+       
+        data[key] === '' ? li.textContent = key + ': нет данных': li.textContent = key + ':' + data[key];
     }
-
-    const ulList = (() => {
-        let arr = [];
-        for (const key in data) {
-            data[key]==='' ? arr.push(key + ": " + 'нет данных') : arr.push(key + ": " + data[key] )
-        }
-        return arr;
-    })()
-
-    function createUl() {
-        const ul = document.createElement('ul');
-        document.body.prepend(ul);
-        let index = 0;
-
-        return function createLi (array) {
-            const arr = array;
-            const li = document.createElement('li');
-            ul.append(li);
-            li.textContent = arr[index];
-            index++
-            if(index < array.length) return createLi(arr);
-         }
-        
-    }
-    const createList = createUl();
-
-    createList(ulList);
-
 
 }()
